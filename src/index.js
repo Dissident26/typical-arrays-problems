24e03b23b3d2a16332a269a5e331e6ef2bf02d6b
Dissident26 <60570678+Dissident26@users.noreply.github.com>
2020-03-17 16:56:56 +0300

    fear: add solution
    add solution for typical array problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (arr) {
+  if ( arr == '' || arr == undefined ) {
+    return 0;
+  } else {
+    return Math.min.apply( null, arr )
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (arr) {
+  if ( arr == '' || arr == undefined ) {
+    return 0;
+  } else {
+    return Math.max.apply( null, arr ) 
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (arr) {
+  if ( arr == '' || arr == undefined ) {
+    return 0;
+  } else {
+    let summ = 0;
+    for ( let i = 0; i < arr.length; i++ ) {
+      summ += arr[i];
+    } return summ / arr.length;
+  }
 }
